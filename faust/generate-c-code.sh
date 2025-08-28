734ac2c86b9c71863763cf7a4e995058c1150602
PowerUser64 <blake@blakenorth.net>
2025-08-28 03:34:56 -0700

    update scripts

--- faust/generate-c-code.sh
+++ faust/generate-c-code.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-faust ./tonebender.dsp -o ./tonebender.gen.c -lang c
+# generate code to tmp file
+faust ./tonebender.dsp -o ./tonebender.gen.c.tmp -lang c
+
+# combine to actual file, prepending faust header
+cat <(echo '#include "faust.h"') ./tonebender.gen.c.tmp > ./tonebender.gen.c
+
+# cleanup
+rm ./tonebender.gen.c.tmp
